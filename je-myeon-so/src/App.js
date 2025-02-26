d67827ff8eb1d1a2d6f2204cc24c94304a4de8c7
ywoo121 <71367047+ywoo121@users.noreply.github.com>
2025-02-26 19:45:24 +0900

    feat: routing 정리

--- je-myeon-so/src/App.js
+++ je-myeon-so/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// App.tsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Resume from './pages/Resume';
+import Interview from './pages/Interview';
+import Result from './pages/Result';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Resume />} />
+        <Route path="/interview" element={<Interview />} />
+        <Route path="/result" element={<Result/> }/>
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </Router>
   );
 }
 
