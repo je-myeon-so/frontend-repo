20a062251f26bd01f8123197e0e53c7b8e864bbd
ywoo121 <71367047+ywoo121@users.noreply.github.com>
2025-02-26 19:45:40 +0900

    feat: Result 페이지

---
+++ je-myeon-so/src/pages/Result.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Result = () => {
+  return (
+      <Container>
+
+      </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+`;
+
+
+export default Result;
